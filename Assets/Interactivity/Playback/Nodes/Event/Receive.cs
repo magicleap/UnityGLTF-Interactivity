c819168e1800fb15bade69acfb4dbaa276cd6306
Jordan Robinson <thefriendgineer@gmail.com>
2025-03-02 15:09:18 -0500

    Event receive now has the correct behaviour when the output values are accessed prior to the event being triggered.

--- Assets/Interactivity/Playback/Nodes/Event/Receive.cs
+++ Assets/Interactivity/Playback/Nodes/Event/Receive.cs
@@ -7,9 +7,18 @@ namespace UnityGLTF.Interactivity
     {
         private Dictionary<string, IProperty> _outValues = new();
 
+        private int _eventToListenFor;
+
         public EventReceive(BehaviourEngine engine, Node node) : base(engine, node)
         {
             engine.onCustomEventFired += OnEventFired;
+
+            // TODO: Putting things here is helpful for performance as we're caching them but does make runtime editing fail.
+            // Figure out if we want to support runtime editing or if we just want to rebuild the whole graph when the user plays back the interaction.
+            if (!TryGetConfig(ConstStrings.EVENT, out _eventToListenFor))
+                throw new InvalidOperationException("No event provided in the config to listen for.");
+
+            AddDefaultValues();
         }
 
         public override IProperty GetOutputValue(string socket)
@@ -22,15 +31,26 @@ namespace UnityGLTF.Interactivity
 
         private void OnEventFired(int eventIndex, Dictionary<string, IProperty> outValues)
         {
-            if (!TryGetConfig(ConstStrings.EVENT, out int eventToListenFor))
-                throw new InvalidOperationException();
-
-            if (eventIndex != eventToListenFor)
+            if (eventIndex != _eventToListenFor)
                 return;
 
             _outValues = outValues;
 
             TryExecuteFlow(ConstStrings.OUT);
+        }
+
+        private void AddDefaultValues()
+        {
+            var eventData = engine.graph.customEvents[_eventToListenFor];
+
+            EventValue value;
+
+            for (int i = 0; i < eventData.values.Count; i++)
+            {
+                value = eventData.values[i];
+
+                _outValues.Add(value.id, engine.graph.GetDefaultPropertyForType(value.type));
+            }
         }
     }
 }
