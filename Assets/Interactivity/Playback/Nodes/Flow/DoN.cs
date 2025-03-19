f14731e5de5e9dd4f9656de5cd334ef2739bc6f3
Jordan Robinson <thefriendgineer@gmail.com>
2025-03-19 09:35:08 -0400

    Fixed DoN node not being able to have its "n" input value change on a per-execution basis.

--- Assets/Interactivity/Playback/Nodes/Flow/DoN.cs
+++ Assets/Interactivity/Playback/Nodes/Flow/DoN.cs
@@ -1,19 +1,14 @@
 using System;
-using System.Diagnostics.CodeAnalysis;
-using System.Threading;
-using UnityEngine;
 
 namespace UnityGLTF.Interactivity
 {
     public class FlowDoN : BehaviourEngineNode
     {
-
         private int _nTimes;
         private int _currentCount = 0;
 
         public FlowDoN(BehaviourEngine engine, Node node) : base(engine, node)
         {
-            TryEvaluateValue(ConstStrings.N, out _nTimes);
         }
 
         protected override void Execute(string socket, ValidationResult validationResult)
@@ -30,11 +25,16 @@ namespace UnityGLTF.Interactivity
                     {
                         _currentCount++;
                         TryExecuteFlow(ConstStrings.OUT);
-                    } 
+                    }
                     break;
                 default:
                     throw new InvalidOperationException($"Socket {socket} is not a valid input on this DoN node!");
             }
+        }
+
+        public override bool ValidateValues(string socket)
+        {
+            return TryEvaluateValue(ConstStrings.N, out _nTimes);
         }
 
         public override IProperty GetOutputValue(string socket)
