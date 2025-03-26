7bd98d9a8c38ac19382953fed43b0a63a11bc562
Max Maynard <maxwellvmaynard@gmail.com>
2025-03-26 16:06:48 -0700

    Broadened and refined flow node tests to function with assert nodes to evaluate the outcomes. The tests can be further broadened for future flow nodes.

--- Assets/Tests/FlowNodesTests.cs
+++ Assets/Tests/FlowNodesTests.cs
@@ -5,35 +5,40 @@ using UnityGLTF.Interactivity;
 
 public class FlowNodesTests : InteractivityTestsHelpers
 {
-    private void CreateFlowBranchGraph(bool condition)
+    private int tv1 = 1;
+
+    private void CreateFlowBranchGraph<T>(string nodeStr, bool condition, T value, T expectedResult)
     {
         var graph = new Graph();
         graph.AddDefaultTypes();
-
+        
         var onStartNode = graph.CreateNode("event/onStart", Vector2.zero);
-        var flowBranchNode = graph.CreateNode("flow/branch", Vector2.zero);
+        var flowNode = graph.CreateNode(nodeStr, Vector2.zero);
+        var assertTrueNode = graph.CreateNode("debug/assert", Vector2.zero);
+        var assertFalseNode = graph.CreateNode("debug/assert", Vector2.zero);
 
-        flowBranchNode.AddValue("condition", condition);
+        onStartNode.AddFlow(ConstStrings.OUT, flowNode, ConstStrings.IN);
+        flowNode.AddFlow(ConstStrings.TRUE, assertTrueNode, ConstStrings.IN);
+        flowNode.AddFlow(ConstStrings.FALSE, assertFalseNode, ConstStrings.IN);
 
-        onStartNode.AddFlow(ConstStrings.OUT, flowBranchNode, ConstStrings.IN);
-
+        flowNode.AddValue(ConstStrings.CONDITION, condition);
+        assertTrueNode.AddValue(ConstStrings.A, expectedResult);
+        assertTrueNode.AddValue(ConstStrings.B, tv1);
+        assertFalseNode.AddValue(ConstStrings.A, expectedResult);
+        assertFalseNode.AddValue(ConstStrings.B, 0);
+        
         RunTestForGraph(graph, null);
     }
 
-    private void TestBranchResult(bool expectedResult)
+    private void TestFlowResult<T>(string nodeStr, bool condition, T val1, T expectedOutcome)
     {
-        CreateFlowBranchGraph(expectedResult);
+        CreateFlowBranchGraph(nodeStr, condition, val1, expectedOutcome);
     }
 
     [Test]
     public void TestBranch()
     {
-        TestBranchResult(true);
-    }
-
-    [Test]
-    public void TestBranchFalse()
-    {
-        TestBranchResult(false);
+        TestFlowResult("flow/branch", true, tv1, tv1);
+        TestFlowResult("flow/branch", false, tv1, 0);
     }
 }
